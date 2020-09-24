a702bc7199f3d984fc7c3a1998b86b26dd2303e9
zg-626 <1058474495@qq.com>
2020-09-24 17:56:46 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+ <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1"/>
+ <a href="javascript:;">
+ 超级重卡，高效节油
+ </a>
+ <div class="float-right">
+ <a href="/about" >关于</a>
+ </div>
+</footer>

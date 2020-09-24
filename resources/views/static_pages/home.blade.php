31b9e63f634e2ab94b11a54b4a604e3b88069ec5
zg-626 <1058474495@qq.com>
2020-09-24 18:36:38 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,10 +6,10 @@
  你现在所看到的是 <a href="javascript:;">福田戴姆勒社区</a>
  </p>
  <p>
- 一切，将从这里开始。
+ 一切，将从这里开始。
  </p>
  <p>
- <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+ <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
  </p>
  </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+//注册
+route::get('/signup','UsersController@create')->name('signup');

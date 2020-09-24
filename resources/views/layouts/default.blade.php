f87933261efdc3822306a37e0a9fddcc68bd8764
zg-626 <1058474495@qq.com>
2020-09-24 11:28:42 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>@yield('title','web')-社区</title>
+ </head>
+ <body>
+  @yield('content')
+ </body>
+</html>

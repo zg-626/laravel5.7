9793b2d2cd59324de1c6baaebf721c8a74a99e34
zg-626 <1058474495@qq.com>
2020-09-24 17:21:48 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,21 @@
   <meta name="Keywords" content="">
   <meta name="Description" content="">
   <title>@yield('title','web')-社区</title>
+  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
  </head>
  <body>
-  @yield('content')
+	 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		 <div class="container">
+			<a class="navbar-brand" href="/">社区</a>
+			 <ul class="navbar-nav justify-content-end">
+			 <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+			 <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+			 </ul>
+		 </div>
+	 </nav>
+	 <div class="container">
+	   @yield('content')
+	 </div>
  </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
- <h1>主页</h1>
+ <div class="jumbotron">
+ <h1>Hello 欧曼</h1>
+ <p class="lead">
+ 你现在所看到的是 <a href="javascript:;">福田戴姆勒社区</a>
+ </p>
+ <p>
+ 一切，将从这里开始。
+ </p>
+ <p>
+ <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+ </p>
+ </div>
 @stop
